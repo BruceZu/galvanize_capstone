7b3f80c11efc8133d1f1d0c8192426457b58fff7
magdiel85281 <magdiel.delgadillo@gmail.com>
2018-12-03 22:29:54 -0700

    modified function to write page html to mongo

--- votes/tablereader_js.py
+++ votes/tablereader_js.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 import pprint 
 import pandas as pd 
 import copy
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup
 import requests
 
 from selenium import webdriver
@@ -16,55 +16,64 @@ import warnings
 
 warnings.filterwarnings('ignore')
 
-
-# create function get_page_data(site_url)
 def page_to_mongo(site_url, collection_name):
     # send GET request using selenium (sites in javascript) and check status
     option = webdriver.ChromeOptions()
     option.add_argument(' - incognito')
+    option.add_argument('--headless')
 
     browser = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver', chrome_options=option)
 
     req = requests.get(site_url)
     print('_______________')
     print('_______________')
+    print(site_url)
     print('Request Status Code: {}'.format(req.status_code))
     if req.status_code == 200:
-        # add page html to mongo
-        collection_name.insert_one('lxml': req.content)
-
-    # render in browser and parse the html with BeautifulSoup
-    browser.get(site_url)
-    soup = bs(browser.page_source, 'lxml')
-    print('--------------')
-    print(site_url)
-    print(soup.title)
-    # print(soup.prettify())
-
-    # table of bills are in ol class
-    # navigate to find bill info
-    div = soup.find('div', {'class':'search-column-main'})
-    table = div.find('ol')
-    # print(table.prettify())
-
-    # iterate though each li class expanded to get rows
-    rows = table.find_all('li', {'class':'expanded'})
-#     print(rows[0].prettify())
-
-    return rows
-
-
+        # add page page html to mongo
+        collection_name.insert_one({'lxml': req.content})
+        # print result of load
+        with open('data/load_results.txt', 'a') as f:
+            f.writelines('{}, {}\n'.format(site_url, req.status_code))
+        f.close()
+        
     else: 
-        print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-        print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
         print('failed to get {}'.format(site_url))
+        # print result of load
+        with open('data/load_results.txt', 'a') as f:
+            f.writelines('{}, {}\n'.format(site_url, req.status_code))
+        f.close()
+    
+    return None
+
+        
+        
+# # need function to parse data from mongo
+#     # render in browser and parse the html with BeautifulSoup
+#         browser.get(site_url)
+#         soup = BeautifulSoup(browser.page_source, 'lxml')
+#         print('--------------')
+#         print(site_url)
+#         print(soup.title)
+#         # print(soup.prettify())
+
+#         # table of bills are in ol class
+#         # navigate to find bill info
+#         div = soup.find('div', {'class':'search-column-main'})
+#         table = div.find('ol')
+#         # print(table.prettify())
+
+#         # iterate though each li class expanded to get rows
+#         rows = table.find_all('li', {'class':'expanded'})
+#     #     print(rows[0].prettify())
+
+#     #     return rows  #no need to return anything if loading html into mongo
+
+
         
 
 # function to add items to mongo
-def add_to_mongo(rows):
-    
-
-
+def parse_to_mongo(rows, collection_name):
     # store each row as key-value pair in a dictionary
     empty_row = {'bill_id': None, 
                 'bill_url': None, 
@@ -100,23 +109,28 @@ def add_to_mongo(rows):
     #     all_rows.append(new_row)
     
     #     store info in mongo
-        pages.insert_one(new_row)
+        collection_name.insert_one(new_row)
 
+        
+        
+
+######################
+
+        
 if __name__ == '__main__':
     # initialize mongo driver to add items as we iterate through them
     client = MongoClient('mongodb://localhost:27017/')
     db = client.bills
     pages = db.pages
 
+        
     # the 101st Congress (1989 - 1990) starts on pg 1011 for pageSize=250
     site_url_root = 'https://www.congress.gov/search?q={%22source%22:%22legislation%22}&pageSize=250'
-    for i in range(1, 1011):
+    for i in range(1, 200):
         site_url = site_url_root + '&page={}'.format(i)
-#         print(site_url)
-        sleep(20)
+        sleep(5)
         rows = page_to_mongo(site_url, pages)
 #         add_to_mongo(rows)
 
-    # all_rows[-5:]
 
-    print('Count of lines loaded: {}'.format(pages.find().count()))
+    print('Count of pages loaded: {}'.format(pages.find().count()))
