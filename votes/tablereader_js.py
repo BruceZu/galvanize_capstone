677906b9203a340b6671f6ca8163f70efe644dca
magdiel85281 <magdiel.delgadillo@gmail.com>
2018-12-04 09:28:32 -0700

    added timestamp to log

--- votes/tablereader_js.py
+++ votes/tablereader_js.py
@@ -4,6 +4,7 @@ import pandas as pd
 import copy
 from bs4 import BeautifulSoup
 import requests
+import datetime
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -25,23 +26,25 @@ def page_to_mongo(site_url, collection_name):
     browser = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver', chrome_options=option)
 
     req = requests.get(site_url)
+    stat_code = req.status_code
+    tstamp = datetime.datetime.now().strftime('%m-%d-%Y %H:%M:%S')
     print('_______________')
     print('_______________')
     print(site_url)
-    print('Request Status Code: {}'.format(req.status_code))
+    print('Request Status Code: {}'.format(stat_code))
     if req.status_code == 200:
         # add page page html to mongo
         collection_name.insert_one({'lxml': req.content})
         # print result of load
         with open('data/load_results.txt', 'a') as f:
-            f.writelines('{}, {}\n'.format(site_url, req.status_code))
+            f.writelines('{}, {}, {}\n'.format(site_url, stat_code, tstamp))
         f.close()
         
     else: 
         print('failed to get {}'.format(site_url))
         # print result of load
-        with open('data/load_results.txt', 'a') as f:
-            f.writelines('{}, {}\n'.format(site_url, req.status_code))
+        with open('data/logs/load_results.txt', 'a') as f:
+            f.writelines('{}, {}, {}\n'.format(site_url, stat_code, tstamp))
         f.close()
     
     return None
@@ -72,44 +75,44 @@ def page_to_mongo(site_url, collection_name):
 
         
 
-# function to add items to mongo
-def parse_to_mongo(rows, collection_name):
-    # store each row as key-value pair in a dictionary
-    empty_row = {'bill_id': None, 
-                'bill_url': None, 
-                'congress_id': None,
-                'desc': None,
-                'sponsor': None, 
-    #             'cosponsors': None,  #requires navigation to another url and extracting names from table
-                'committee': None, 
-                'bill_status': None,
-                'body': None   #requires navigation to another url
-                }
+# # function to add items to mongo
+# def parse_to_mongo(rows, collection_name):
+#     # store each row as key-value pair in a dictionary
+#     empty_row = {'bill_id': None, 
+#                 'bill_url': None, 
+#                 'congress_id': None,
+#                 'desc': None,
+#                 'sponsor': None, 
+#     #             'cosponsors': None,  #requires navigation to another url and extracting names from table
+#                 'committee': None, 
+#                 'bill_status': None,
+#                 'body': None   #requires navigation to another url
+#                 }
 
 
-    # all_rows = []
+#     # all_rows = []
 
-    # iterate through each of the 'rows' to fill in the 'columns'
-    for row in rows:
-        new_row = copy.copy(empty_row)
+#     # iterate through each of the 'rows' to fill in the 'columns'
+#     for row in rows:
+#         new_row = copy.copy(empty_row)
 
-        columns = row.find_all('a')
-        new_row['bill_id'] = columns[0].text.strip()
-        new_row['bill_url'] = columns[0]['href'].strip()
-        new_row['sponsor'] = columns[1].text.strip()
+#         columns = row.find_all('a')
+#         new_row['bill_id'] = columns[0].text.strip()
+#         new_row['bill_url'] = columns[0]['href'].strip()
+#         new_row['sponsor'] = columns[1].text.strip()
 
-        columns = row.find_all('span')
-        new_row['congress_id'] = columns[1].text.strip().split()[2]
-        new_row['desc'] = columns[2].text
-        new_row['committee'] = columns[4].text.strip()[12:]
+#         columns = row.find_all('span')
+#         new_row['congress_id'] = columns[1].text.strip().split()[2]
+#         new_row['desc'] = columns[2].text
+#         new_row['committee'] = columns[4].text.strip()[12:]
 
-        columns = row.find_all('p')
-        new_row['bill_status'] = columns[0].text.strip()[25:]
+#         columns = row.find_all('p')
+#         new_row['bill_status'] = columns[0].text.strip()[25:]
 
-    #     all_rows.append(new_row)
+#     #     all_rows.append(new_row)
     
-    #     store info in mongo
-        collection_name.insert_one(new_row)
+#     #     store info in mongo
+#         collection_name.insert_one(new_row)
 
         
         
@@ -126,7 +129,7 @@ if __name__ == '__main__':
         
     # the 101st Congress (1989 - 1990) starts on pg 1011 for pageSize=250
     site_url_root = 'https://www.congress.gov/search?q={%22source%22:%22legislation%22}&pageSize=250'
-    for i in range(501, 1012):
+    for i in range(1, 1011):
         site_url = site_url_root + '&page={}'.format(i)
         sleep(5)
         rows = page_to_mongo(site_url, pages)
