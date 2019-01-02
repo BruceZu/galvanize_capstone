6157a10948e3b2f60fa60772cc0eae7e806dbb62
magdiel85281 <magdiel.delgadillo@gmail.com>
2019-01-02 19:41:25 +0000

    creating script to update mongo with new info

---
+++ src/get_new_bill_info.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from pymongo import MongoClient
+import copy
+from bs4 import BeautifulSoup
+import requests
+from random import randint
+from time import sleep
+import threading
