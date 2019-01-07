4b34f0e81f88031f7eb3a5c83eef650d4a90910b
magdiel85281 <magdiel.delgadillo@gmail.com>
2019-01-07 23:36:57 +0000

    updates to flask app and associated files

--- web/mongo_to_flask.py
+++ web/mongo_to_flask.py
@@ -29,28 +29,46 @@ app = Flask(__name__)
 # Set up Mongo
 client = MongoClient() # defaults to localhost
 db = client.bills
+bill_info = db.bill_info
+predictions = db.predictions
 
+
+
+# welcome page
+@app.route('/')
+def welcome_page():
+    return 'Hello!'
 
 #Controller: Fetch all 
+
+# Fetch predictions
+@app.route('/predictions')
+def show_predictions():
+    # return list of bills still in progress with predicted probabilities
+    documents = predictions.find({'$query': {}, '$orderby': {'probability': -1}})
+    
+    return render_template('predictions_table.html', rows=list(documents))
+    
+    
 
 
 # Fetch bills/laws by leg_id
 @app.route("/leg_id/<id>")
 def leg_id(id):
     # return search results sorted by most recent
-    row = db.bill_details.find({'$query': {"leg_id": id}, 
+    documents = db.bill_info.find({'$query': {"leg_id": id}, 
                                 '$orderby': {'congress_id': -1}})
     
-    return render_template('table.html', rows=list(row))
+    return render_template('table.html', rows=list(documents))
 
 
 # Fetch bills/laws by congress_id
 @app.route("/congress_id/<id>")
 def congress_id(id):
-    row = db.bill_details.find({'$query': {"congress_id": id}, 
+    documents = db.bill_info.find({'$query': {"congress_id": id}, 
                                 '$orderby': {'intro_date': -1}})
     
-    return render_template('table.html', rows=list(row))
+    return render_template('table.html', rows=list(documents))
 
 
 
@@ -59,11 +77,11 @@ def congress_id(id):
 # Fetch bill in current congress that originated in the House
 @app.route("/house_bills")
 def house_bills():
-    bills = db.bill_details.find({'leg_id': {'$regex': '^H'}, 'congress_id': '115th'}, 
+    documents = db.bill_info.find({'leg_id': {'$regex': '^H'}, 'congress_id': '115'}, 
                                  sort = [('intro_date', -1)])  
     
-    bill_count = bills.count()
-    return render_template('bills.html', bills=bills, origin='House', bill_count=bill_count)
+    bill_count = documents.count()
+    return render_template('bills.html', bills=documents, origin='House', bill_count=bill_count)
     
     
     
@@ -72,11 +90,11 @@ def house_bills():
 # Fetch bill in current congress that originated in the House
 @app.route("/senate_bills")
 def senate_bills():
-    bills = db.bill_details.find({'leg_id': {'$regex': '^S'}, 'congress_id': '115th'}, 
+    documents = db.bill_info.find({'leg_id': {'$regex': '^S'}, 'congress_id': '115'}, 
                                  sort = [('intro_date', -1)])  
     
-    bill_count = bills.count()
-    return render_template('bills.html', bills=bills, origin='Senate', bill_count=bill_count)
+    bill_count = db.bill_info.count_documents({'leg_id': {'$regex': '^S'}, 'congress_id': '115'})
+    return render_template('bills.html', bills=documents, origin='Senate', bill_count=bill_count)
 
 
 # Fetch an atomic record
@@ -87,9 +105,9 @@ def bill_info():
     cong_id = request.args.get('congress_id')
     
     
-    bill = db.bill_details.find_one({'leg_id': bill_id, 'congress_id': cong_id})
+    doc = db.bill_info.find_one({'leg_id': bill_id, 'congress_id': cong_id})
 
-    return render_template('bill.html', bill=bill)
+    return render_template('bill.html', bill=doc)
 
 
 
