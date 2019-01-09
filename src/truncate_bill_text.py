2eac2a5d977b0a38e2edfad5c17e33c1b8d6b555
magdiel85281 <magdiel.delgadillo@gmail.com>
2019-01-09 19:19:37 +0000

    updated to pull from logs, functionalized

--- src/truncate_bill_text.py
+++ src/truncate_bill_text.py
@@ -25,6 +25,48 @@ def update_mongo_bill_text(leg_id, cong_id, bill_text_trunc, collection):
     collection.update_one({'leg_id': leg_id, 'congress_id': cong_id}, {'$set': {'bill_text': bill_text_trunc}})
 
 
+def truncate_bill_body(bill_text):
+    '''
+    
+    '''
+    # search through headers to get index
+    if ('A BILL' in bill_text[:5000]):
+        header_text = 'A BILL'
+
+    elif ('A Bill' in bill_text[:5000]):
+        header_text = 'A Bill'            
+
+    elif ('JOINT RESOLUTION' in bill_text[:5000]): 
+        header_text = 'JOINT RESOLUTION'
+
+    elif ('Joint Resolution' in bill_text[:5000]):
+        header_text = 'Joint Resolution'
+
+    elif ('An Act' in bill_text[:5000]): 
+        header_text = 'An Act'
+
+    elif ('AN ACT' in bill_text[:5000]): 
+        header_text = 'AN ACT'
+
+    else: 
+        header_text = 'ing Office'
+
+    text_start = bill_text.find(header_text)
+
+    # truncate the bill_text to remove header
+    bill_text_trunc = bill_text[text_start:].split(' ', 2)[2]
+
+
+    # truncate bill text to remove footer
+    if 'LEGISLATIVE HISTORY' in bill_text_trunc:
+        text_end = bill_text_trunc.find('LEGISLATIVE HISTORY')
+        bill_text_trunc = bill_text_trunc[:text_end].rsplit('Approved')[0]
+
+    return bill_text_trunc    
+    
+    
+    
+
 if __name__ == '__main__':
     client = MongoClient()
     db = client.bills
@@ -33,68 +75,24 @@ if __name__ == '__main__':
     # retrieve logs where the bill text has changed when get_bill_text was run
     log_path = '/home/ubuntu/galvanize_capstone/data/logs/mongo_updates.jsonl'
     logs = read_jsonl_file(log_path)
+    
+    today = date.today.isoformat()
 
     for log in logs:
+        # check to see if the body was updated with get_bill_text today
         if 'body' in log.keys():
-            print('----------------------')
-            print('----------------------')
-            cong_id = log['congress_id']
-            leg_id = log['leg_id'] 
-            print('The bills text for Congress ID {}, {} has changed. Updating truncated text'.format(cong_id, leg_id))
+            if log['body']['date'] == today:
+                cong_id = log['congress_id']
+                leg_id = log['leg_id'] 
+                print('The bills text for Congress ID {}, {} has changed. Updating truncated text'.format(cong_id, leg_id))
 
+                # use cong_id and leg_id in log to pull bill text from Mongo and clip it
+                doc = bill_info.find_one({'congress_id': cong_id, 'leg_id': leg_id})
+                bill_text_clipped = truncate_bill_body(doc['body'])
 
-    # get doc count to show status
-    doc_count = bill_info.count_documents({'body': {'$regex': '(.+)'}, 'bill_text': None})
-    print('-------------------')
-    print('There are {} bills needing truncated text'.format(doc_count))
-    
-    # retrieve Mongo documents
-    documents = bill_info.find({'body': {'$regex': '(.+)'}, 'bill_text': None})
+                # update Mongo
+                update_mongo_bill_text(leg_id, cong_id, bill_text_clipped, bill_info)     
 
-    i = 0
-    
-    for doc in documents:
-        leg_id = doc['leg_id']
-        cong_id = doc['congress_id']
-        bill_text = doc['body']
-
-        # search through headers to get index
-        if ('A BILL' in bill_text[:5000]):
-            header_text = 'A BILL'
-
-        elif ('A Bill' in bill_text[:5000]):
-            header_text = 'A Bill'            
-
-        elif ('JOINT RESOLUTION' in bill_text[:5000]): 
-            header_text = 'JOINT RESOLUTION'
-
-        elif ('Joint Resolution' in bill_text[:5000]):
-            header_text = 'Joint Resolution'
-
-        elif ('An Act' in bill_text[:5000]): 
-            header_text = 'An Act'
-
-        elif ('AN ACT' in bill_text[:5000]): 
-            header_text = 'AN ACT'
-
-        else: 
-            header_text = 'ing Office'
-
-        text_start = bill_text.find(header_text)
-
-        # truncate the bill_text to remove header
-        bill_text_trunc = bill_text[text_start:].split(' ', 2)[2]
-
-
-        # truncate bill text to remove footer
-        if 'LEGISLATIVE HISTORY' in bill_text_trunc:
-            text_end = bill_text_trunc.find('LEGISLATIVE HISTORY')
-            bill_text_trunc = bill_text_trunc[:text_end].rsplit('Approved')[0]
-
-        # update Mongo
-        update_mongo_bill_text(leg_id, cong_id, bill_text_trunc, bill_info)     
-        
-        # show status
-        if i%20 == 0:
-            print('{:.2f}% complete truncating bill text'.format(i / doc_count))
-        i += 1
+            
+            
+            
